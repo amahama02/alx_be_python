e27417ac3a8c39a138fb810434585b09276edbb0
Amah Gordon <amah.gordon@gmail.com>
2025-05-22 08:21:51 +0000

    add basic_operation.py for fundamantal calculations

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,17 @@
+# We define the two numbers we will use
+number1 = 10
+number2 = 5
+
+# We calculate the sum (addition)
+sum_result = number1 + number2
+
+# We calculate the difference (subtracting number2 from number1)
+difference_result = number1 - number2
+
+# We calculate the product (multiplication)
+product_result = number1 * number2
+
+# We display the results. The 'f' before the quotes allows variables to be directly included in the text.
+print(f"Sum of {number1} and {number2} is {sum_result}")
+print(f"Difference of {number1} and {number2} is {difference_result}")
+print(f"Product of {number1} and {number2} is {product_result}")
